0498ed8f114d6bf9cc6d353a44041049ff648e6f
dima korshunov <dimon4ik142@yandex.ru>
2023-02-02 16:42:34 +0300

    TASK1

---
+++ TASK1/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int GetValue(string msg)
+{
+    System.Console.Write(msg);
+    string v = Console.ReadLine();
+    return Convert.ToInt32(v);
+}
+
+bool CheckNumber(int b)
+{
+    if (b ==0)
+    {
+        System.Console.WriteLine("Любое число в нулевой степени равно единице");
+        return false;
+    }
+    return true;
+}
+long MyPow(int a, int b)
+{
+    int degree = 1;
+    long result = a;
+    while (degree < Math.Abs(b))
+    {
+        result *= a;
+        degree++;
+    }
+    return result;
+}
+
+int a = GetValue("введите первое число:");
+int b = GetValue("введите второе число:");
+
+if (CheckNumber(b))
+{
+    long result = MyPow(a, b);
+    System.Console.WriteLine($"{a} ^ {b} = {result}");
+}
